5a5915c14248832b62c56bbde729052e3eb89e8a
Benjamin Spreng <benjamin.spreng@physik.uni-augsburg.de>
2022-12-05 19:27:05 -0800

    add install script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='casimir_planeplane',
+   version='0.0.0',
+   author='Benjamin Spreng',
+   author_email='sprengjamin@gmail.com',
+   packages=['casimir_planeplane'],
+   license='LICENSE.txt',
+   description='Casimir interaction involving two planes',
+   long_description=open('README.txt').read(),
+   install_requires=[],
+)
